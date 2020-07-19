6c47e985388c6aac86e52b3fe8c6044844fe885d
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-07-19 20:20:42 +0900

    オプションを使用した検索をコミット

--- src/1-3.js
+++ src/1-3.js
@@ -7,3 +7,19 @@ console.log('ABC_ABC_ABC'.match(/ABC/g));
 // iオプション
 console.log('ABC'.match(/abc/));
 console.log('ABC'.match(/abc/i));
+
+// mオプション
+console.log("ABC\nDEF".match(/^D/));
+console.log("ABC\nDEF".match(/^D/m));
+
+// yオプション
+var exp = /ABC/y;
+console.log(exp.lastIndex);
+console.log(exp.test('ABC'));
+
+exp.lastIndex = 1;
+console.log(exp.test('ABC'));
+console.log('ABC'.match(exp));
+
+// gオプションとiオプションの組み合わせ
+console.log('ABC_abc_ABC'.match(/ABC/gi));
